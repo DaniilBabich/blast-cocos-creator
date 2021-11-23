05889bbdcf7fcacba94d587be69f36396da120b9
Daniil Babich <daniil.babich.1990@yandex.ru>
2021-11-24 02:03:27 +0500

    build

---
+++ src/settings.3ff1e.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/game.fire",orientation:"",jsList:[],bundleVers:{internal:"efe26",main:"e4711"}};
